79bedf8fcb3c7cf7582804fdc0a48a3738dceebe
ViktoryiaShylko <vika.shylko@mail.ru>
2025-05-24 15:04:42 +0300

    fixed cards

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,12 +1,15 @@
 function HeroCard(props) {
+  const { name, universe, alterego, occupation, superpowers, url, info } =
+    props.hero;
   return (
     <div className="card">
-      <h2 className="card-title">{props.title}</h2>
-      <img src={props.imgLink} alt={props.title} />
-      <p className="universe">{props.universe}</p>
-      <p className="alterego">{props.alterego}</p>
-      <p className="superpower">{props.superpower}</p>
-      <p className="description">{props.description}</p>
+      <h2 className="name">{name}</h2>
+      <p className="universe">{universe}</p>
+      <p className="alterego">{alterego}</p>
+      <p className="occupation">{occupation}</p>
+      <p className="superpowers">{superpowers}</p>
+      <img src={url} alt={name} />
+      <p className="info">{info}</p>
     </div>
   );
 }
